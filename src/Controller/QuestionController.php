236169349ad62623c3f05fd9e3eaa01a26e188f3
Florian Gerard <florian.gerard06@gmail.com>
2025-05-13 21:28:06 +0200

    Ajout controllers wonder part 1

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -8,12 +8,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/question', name: 'question_')]
 class QuestionController extends AbstractController
 {
-    #[Route('/question/ask', name: 'question_form')]
+    #[Route('/ask', name: 'form')]
     public function index(Request $request): Response
     {
+
         $formQuestion = $this->createForm(QuestionType::class);
+
         $formQuestion->handleRequest($request);
 
         if ($formQuestion->isSubmitted() && $formQuestion->isValid()) {
