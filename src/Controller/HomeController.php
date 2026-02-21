148b0457daaa86036d038236792b84678e7556bb
Florian Gerard <florian.gerard06@gmail.com>
2026-02-21 08:25:46 +0100

    avancement projet wonder (quora)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,56 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
 use App\Repository\QuestionRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
-    public function index(QuestionRepository $questionRepository): Response
-    {
-        $questions = $questionRepository->findAll();
 
-        $questions = [
-            [
-                'id' => '1',
-                'title' => 'Je suis une super question',
-                'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, adipisci. Libero aperiam dolores excepturi, quidem maxime accusantium inventore. Illum, odio dolores! Ullam omnis veritatis laborum, animi inventore nostrum optio voluptates.',
-                'rating' => 20,
-                'author' => [
-                    'name' => 'Jean Dupont',
-                    'avatar' => 'https://randomuser.me/api/portraits/men/52.jpg'
-                ],
-                'nbrOfResponse' => 15
-            ],
-            [
-                'id' => '2',
-                'title' => 'Je suis une super question',
-                'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, adipisci. Libero aperiam dolores excepturi, quidem maxime accusantium inventore. Illum, odio dolores! Ullam omnis veritatis laborum, animi inventore nostrum optio voluptates.',
-                'rating' => 0,
-                'author' => [
-                    'name' => 'Julie Dupont',
-                    'avatar' => 'https://randomuser.me/api/portraits/women/44.jpg'
-                ],
-                'nbrOfResponse' => 15
-            ],
-            [
-                'id' => '3',
-                'title' => 'Je suis une super question',
-                'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, adipisci. Libero aperiam dolores excepturi, quidem maxime accusantium inventore. Illum, odio dolores! Ullam omnis veritatis laborum, animi inventore nostrum optio voluptates.',
-                'rating' => -15,
-                'author' => [
-                    'name' => 'Jean Dupont',
-                    'avatar' => 'https://randomuser.me/api/portraits/men/46.jpg'
-                ],
-                'nbrOfResponse' => 15
-            ],
-        ];
+    #[Route('/', name: 'home')]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $questionRepo = $doctrine->getRepository(Question::class);
+        $questions = $questionRepo->findAll();
 
         return $this->render('home/index.html.twig', [
             'questions' => $questions
         ]);
     }
 }
+
+// 'https://randomuser.me/api/portraits/women/44.jpg'
